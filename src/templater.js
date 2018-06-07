a226425b541d0ea37d95a851495135a5de781749
ohladkov <ohladkov@justcoded.co>
2018-06-07 17:45:02 +0300

    stage-3

--- src/templater.js
+++ src/templater.js
@@ -1,18 +1,43 @@
 const Templater = {
   tags: [],
   templates: [],
+
   addTag(tag, template) {
     this.tags.push(tag);
     this.templates.push(template);
   },
-  run() {
-    this.tags.forEach((tag, i) => {
-      const domTag = [...document.querySelectorAll(tag)];
-      const template = this.templates[i];
 
-      domTag.forEach((el) => {
-        el.outerHTML = template;
-      })
-    })
+  render(tags,templates) {
+    tags.forEach((tag, i) => {
+      const domTags = [...document.querySelectorAll(tag)];
+      let template = templates[i];
+      const matches = findMatches(template);
+
+      domTags.forEach((tag) => {
+        matches.forEach((match) => {
+          const attr = match.slice(2, match.length-2);
+          if ( attr === 'html' ) {
+            template = template.replace(match, tag.innerHTML);
+          }
+          template = template.replace(match, tag.getAttribute(attr));
+          return template;
+        });
+        tag.outerHTML = template;
+      });
+    });
+
+    function findMatches(str) {
+      const re = /({{.+?}})/gm;
+      const matchArr = [];
+      let curMatch;
+      while(curMatch = re.exec(str)) {
+        matchArr.push(curMatch[1]);
+      }
+      return matchArr;
+    }
+  },
+
+  run() {
+    this.render(this.tags, this.templates);
   }
 }
