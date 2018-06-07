6efad20d7d235757771346a3aaa8e695e172296e
ohladkov <ohladkov@justcoded.co>
2018-06-07 17:46:59 +0300

    stage-4

--- src/templater.js
+++ src/templater.js
@@ -1,43 +1,61 @@
-const Templater = {
-  tags: [],
-  templates: [],
+(function($) {
 
-  addTag(tag, template) {
-    this.tags.push(tag);
-    this.templates.push(template);
-  },
+  $.fn.templater = function(options) {
 
-  render(tags,templates) {
-    tags.forEach((tag, i) => {
-      const domTags = [...document.querySelectorAll(tag)];
-      let template = templates[i];
-      const matches = findMatches(template);
+    const self = this;
 
-      domTags.forEach((tag) => {
-        matches.forEach((match) => {
-          const attr = match.slice(2, match.length-2);
-          if ( attr === 'html' ) {
-            template = template.replace(match, tag.innerHTML);
-          }
-          template = template.replace(match, tag.getAttribute(attr));
-          return template;
+    const opts = $.extend( {}, $.fn.templater.defaults, options );
+    const tags = [];
+    const templates = [];
+
+
+    for (const key in opts.tags) {
+      if (opts.tags.hasOwnProperty(key)) {
+        tags.push(key);
+        templates.push(opts.tags[key]);
+      }
+    }
+
+
+    function render() {
+      tags.forEach((tag, i) => {
+        const domTags = [...self[0].querySelectorAll(tag)];
+        let template = templates[i];
+        const matches = findMatches(template);
+
+        domTags.forEach((tag) => {
+          matches.forEach((match) => {
+            const attr = match.slice(2, match.length-2);
+            if ( attr === 'html' ) {
+              template = template.replace(match, tag.innerHTML);
+            }
+            template = template.replace(match, tag.getAttribute(attr));
+            return template;
+          });
+
+          tag.outerHTML = template;
         });
-        tag.outerHTML = template;
       });
-    });
+    }
 
     function findMatches(str) {
       const re = /({{.+?}})/gm;
       const matchArr = [];
       let curMatch;
+
       while(curMatch = re.exec(str)) {
         matchArr.push(curMatch[1]);
       }
+
       return matchArr;
     }
-  },
 
-  run() {
-    this.render(this.tags, this.templates);
-  }
-}
+    this.each(function() {
+      render();
+    });
+
+    return this;
+  };
+
+
+})(jQuery);
